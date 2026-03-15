be8fbe924b016a358dfbf196e6db2297fe1d1399
Juan Cruz Dal Bello <juancruzdalbello17@gmail.com>
2026-03-15 17:32:33 -0300

    uso de la interfaz Experimento en main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from core.gestor_de_ventana import GestorDeVentana
-# from experimento_bowl.experimento_bowl import Experimento
-from experimento_nback.experimento_nback import ExperimentoNBack
+from core.experimento import Experimento
+from experimento_bowl.experimento_bowl import ExperimentoBowl
+# from experimento_nback.experimento_nback import ExperimentoNBack
 
 def main() -> None:
     gestor_ventana: GestorDeVentana = GestorDeVentana()
-    experimento_bowl = ExperimentoNBack()
-    experimento_bowl.ejecutar()
+    experimento: Experimento = ExperimentoBowl(gestor_ventana)
+    experimento.ejecutar()
 
 if __name__ == "__main__":
     main()
